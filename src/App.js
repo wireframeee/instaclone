723a9ec4ea64f038f496659379c869e922be22bc
wireframeee <brightly2469@gmail.com>
2024-04-23 17:39:52 +0530

    feed page skeleton and router setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <div className='topBar'></div>
+      <div className='stories'>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+        <div className='story'></div>
+      </div>
+      <div className='posts'>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+        <div className="post"></div>
+      </div>
+      <div className='bottomBar'>
+        <button>home</button>
+        <button>explore</button>
+        <button>upload</button>
+        <button>reels</button>
+        <button>profile</button>
+      </div>
     </div>
   );
 }
